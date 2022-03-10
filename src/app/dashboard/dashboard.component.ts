7c42ef2637707377917df98d58e7c36cc8516a97
Moumita Dey <mdey@tulieservices.com>
2022-03-10 21:50:25 +0530

    Dashboard by Moumita

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,4 +12,14 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  status: boolean = false;
+  clickDashboardSidebar(){
+      this.status = !this.status;        
+  }
+overlayIsVisited = false;
+overlayCheckVisited() {
+   // reverse the value of property
+   this.overlayIsVisited = !this.overlayIsVisited;
+   this.status = !this.status;
+}
 }
